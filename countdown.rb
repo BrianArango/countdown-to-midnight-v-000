95d99ae48914faaa74b5cd36ca0bd65f023b12a0
Brian Arango <brianvictus@gmail.com>
2020-07-20 01:53:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,16 @@
-def countdown(seconds_to_midnight)
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
-    seconds_to_midnight -= 1
-  end
-  "HAPPY NEW YEAR!"
+def countdown(countdown_output)
+  while countdown_output > 0
+  puts "#{countdown_output} SECOND(S)!"
+  countdown_output -= 1
+end
+return "HAPPY NEW YEAR!"
 end
 
- def countdown_with_sleep(seconds_to_midnight)
+def count_with_sleep(seconds_to_midnight)
   while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
-    sleep(1)
-    seconds_to_midnight -= 1
-  end
-  "HAPPY NEW YEAR!"
-end	
+  puts "#{seconds_to_midnight} SECOND(S)!"
+  sleep(1)
+  seconds_to_midnight -= 1 
+end
+"HAPPY NEW YEAR!"
+end
